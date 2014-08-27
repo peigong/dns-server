3bbf75ac54136fca6734f88442bea12f293e7e4d
周培公 <peigong@foxmail.com>
2014-08-27 14:26:46 +0800

    增加初始代码

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-dns-server
-==========
+# DNS服务器 #
 
-DNS服务器
+## 部署服务的目录结构 ##
+
+部署服务需要的目录结构如下：
+
+- **config：**服务的配置目录。
+- **dns：**服务的软链接目录。
+- **release：**版本发布目录。
+
+**建议：**可以部署在目标服务器的`/home/localad/dns-server`目录下。
+
+## 服务部署的方法 ##
+
+### 克隆和部署 ###
+
+使用GIT克隆项目代码：
+
+	cd /home/localad/dns-server/release
+	git clone https://github.com/localad/dns-server.git dns-server-{版本号} -b {版本号}
+
+### 建立软连接 ###
+
+	rm -rf /home/localad/dns-server/proxy
+	ln -s /home/localad/dns-server/release/dns-server-{版本号} /home/localad/dns-server/proxy
+
+第一次部署时，需要复制系统配置文件，并设置：
+
+	cp -r /home/localad/dns-server/release/dns-server-{版本号}/config /home/localad/dns-server
+
+### 启动和停止服务 ###
+
+进入`dns/bin`目录，可以使用以下命令：
+
+- `sh ./server-install.sh`：安装NPM依赖
+- `sh ./dns-server.sh start`：启动
+- `sh ./dns-server.sh stop`：停止
+- `sh ./dns-server.sh restart`：重启
+
+## 公共DNS ##
+
+- CNNIC SDNS：`1.2.4.8` `210.2.4.8`
+- [阿里公共DNS](http://www.alidns.com/)：`223.5.5.5` `223.6.6.6`
+- 114 DNS：`114.114.114.114` `114.114.115.115`
+- Google DNS：`8.8.8.8` `8.8.4.4`
+
+## 服务器DNS的配置 ##
+
+本机的DNS配置信息是在：`/etc/resolv.conf`
+
+## 参考 ##
+
+- [全国 DNS 服务器](http://www.ip.cn/dns.html)
+- [全国内外公共DNS服务器地址分享](http://www.orsoon.com/news/pcnews/15733.html)
---
+++ server/server.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var dns = require('native-dns'),
+  server = dns.createServer();
+
+var config = path.resolve(__dirname, '..', '..', '..', 'config');
+var dict = {}, proxy = '', domains = [], settings = require(path.join(config, 'settings.json'));
+if(settings.proxy){
+  proxy = settings.proxy;
+}
+if(settings.domains){
+  domains = require(path.join(config, 'domains.json'));
+}
+domains.map(function(domain){
+  dict[domain] = 1;
+});
+
+var sendResponse = function(response, domain, ips){
+  ips = ips || [];
+  ips.map(function(ip){
+    response.answer.push(dns.A({
+      name: domain,
+      address: ip,
+      ttl: 600
+    }));
+  });
+  response.send();
+}
+  
+var onMessage = function (request, response) {
+  var domain = request.question[0].name);
+  if (dict.hasOwnProperty(domain) && proxy) {
+    console.log([domain, proxy].join(':'));
+    sendResponse(response, domain, [proxy]);
+  }else{
+    dns.resolve(domain, function (err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        results = results || [];
+        console.log([domain, results.join(',')].join(':'));
+        sendResponse(response, domain, results);
+      }
+    });
+  }
+};
+var onError = function (err, buff, req, res) {
+  console.log(err.stack);
+};
+
+var onListening = function () {
+  console.log('server listening on', this.address());
+};
+
+var onSocketError = function (err, socket) {
+  console.log(err);
+};
+
+var onClose = function () {
+  console.log('server closed', this.address());
+};
+
+server.on('request', onMessage);
+server.on('error', onError);
+server.on('listening', onListening);
+server.on('socketError', onSocketError);
+server.on('close', onClose);
+
+server.serve(53, '127.0.0.1');
+console.log('dns server running!')
